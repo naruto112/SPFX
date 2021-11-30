f79f40fdd7ef4190b053a81180d615d082371ba6
Renato Moschetta <renatorock3@hotmail.com>
2021-11-30 20:06:11 -0300

    código spfx

--- src/webparts/login/components/Login.tsx
+++ src/webparts/login/components/Login.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import styles from "./Login.module.scss";
 import stylesGlobal from "../../../Global.module.scss";
 import { ILoginProps } from "./ILoginProps";
-import { escape } from "@microsoft/sp-lodash-subset";
 import { FiLogIn } from "react-icons/fi";
 import { sp, Web, IWeb } from "@pnp/sp/presets/all";
-import { Link } from "office-ui-fabric-react/lib/Link";
 import * as strings from "LoginWebPartStrings";
+import { Formik, Form, Field } from "formik";
+// import * as Yup from "yup";
 
 export default class Login extends React.Component<ILoginProps, {}> {
   public constructor(props: ILoginProps) {
@@ -17,17 +17,25 @@ export default class Login extends React.Component<ILoginProps, {}> {
   }
 
   public render(): React.ReactElement<ILoginProps> {
-    async function handleLogin(e) {
-      e.preventDefault();
-      let web = Web(this.props.web);
+    // const SignupSchema = Yup.object().shape({
+    //   id: Yup.string()
+    //     .min(2, "Too Short!")
+    //     .max(50, "Too Long!")
+    //     .required("Required"),
+    // });
 
-      try {
-        let items = web.lists.getByTitle("Usuario").items.select("*");
+    function handleLogin() {
+      alert("entrou");
+      // e.preventDefault();
+      // let web = Web(this.props.web);
 
-        console.log(items);
-      } catch (err) {
-        console.log(err);
-      }
+      // try {
+      //   let items = web.lists.getByTitle("Usuario").items.select("*");
+
+      //   console.log(items);
+      // } catch (err) {
+      //   console.log(err);
+      // }
     }
 
     return (
@@ -35,21 +43,52 @@ export default class Login extends React.Component<ILoginProps, {}> {
         <div className={styles["logon-container"]}>
           <section className={styles.form}>
             <img src={require("./assets/logo.svg")} alt="Be The Hero" />
-            <form onSubmit={handleLogin}>
-              <h1>Faça seu Logon</h1>
-              <input
-                placeholder="Sua ID"
-                value={this.props.id}
-                onChange={(e) => this.setState({ id: e.currentTarget.value })}
-              />
-              <button className={stylesGlobal.button} type="submit">
-                Entrar
-              </button>
-              <a className={stylesGlobal["back-link"]} href="/Login/123">
-                <FiLogIn size={16} color="#E02041" />
-                Não tenho cadastro
-              </a>
-            </form>
+            <Formik
+              initialValues={{
+                id: "",
+              }}
+              // validationSchema={SignupSchema}
+              validate={(values) => {
+                const errors = { id: "" };
+                if (!values.id) {
+                  errors.id = "Required";
+                }
+                return errors;
+              }}
+              onSubmit={handleLogin}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <h1>Faça seu Logon</h1>
+                  <input
+                    name="id"
+                    placeholder="Sua ID"
+                    value={values.id}
+                    onChange={handleChange}
+                  />
+                  {errors.id && touched.id && errors.id}
+                  <button
+                    className={stylesGlobal.button}
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Entrar
+                  </button>
+                  <a className={stylesGlobal["back-link"]} href="/Login/123">
+                    <FiLogIn size={16} color="#E02041" />
+                    Não tenho cadastro
+                  </a>
+                </form>
+              )}
+            </Formik>
           </section>
           <img src={require("./assets/heroes.png")} alt="Heroes" />
         </div>
